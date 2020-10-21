47d717578a612259a02a8dcfd5b58b1ae7cc2ce6
armsa <armsa@DESKTOP-508V2FR>
2020-10-21 10:16:53 +0300

    1_hafta
    1. hafta ile ilgili çalışmalar

---
+++ 1_hafta/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _1_hafta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KullaniciGirisi.Kullanici();
+            Console.WriteLine("");
+            
+            Ortalama.OrtalamaHesabi();
+            Console.WriteLine("");
+            
+            KitleEndeksi.VucutKitle();
+            Console.WriteLine("");
+
+
+            Console.ReadKey();
+        }
+
+       
+    }
+}
---
+++ 1_hafta/kullanici_girisi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace _1_hafta
+{
+    class KullaniciGirisi
+    {
+        public static void Kullanici()
+        {
+
+            for (int i = 0; i < 1;)
+            {
+                Console.Write("Kullanıcı Adı:");
+                string kullanici_adi = Console.ReadLine();
+                Console.Write("Şifre: ");
+                string sifre = Console.ReadLine();
+
+                if (kullanici_adi == "armsay" && sifre == "12345")
+                {
+                    Console.WriteLine("Giriş Başarılı");
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine("Kullanıcı adı veya şifre geçersiz.");
+                }
+            }
+            
+
+
+        }
+    }
+}
